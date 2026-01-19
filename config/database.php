51196ac7f20158fe8cae1217651a4648027ea77a
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-01-19 16:23:28 +0100

    the database config

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'driver'   => $_ENV['DB_CONNECTION'],
+    'host'     => $_ENV['DB_HOST'],
+    'port'     => $_ENV['DB_PORT'],
+    'database' => $_ENV['DB_DATABASE'],
+    'username' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD'],
+];
