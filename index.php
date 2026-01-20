41b3b7b9af719392b69b3c714ea73393ee8b81d6
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-01-20 12:55:11 +0100

    models users

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ .  '/../src/core/Database.php';
+
+$envFile = __DIR__ . '/../.env';
+if (file_exists($envFile)) {
+    $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) continue;
+        [$key, $value] = explode('=', $line, 2);
+        $_ENV[trim($key)] = trim($value);
+    }
+}
---
+++ src/models/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace src\Models;
+
+abstract class  User{
+
+    protected int $id;
+    protected string $name;
+    protected string $email;
+    protected string $password;
+    protected string $role;
+
+    public function __construct(string $name,string $email,string $password,string $role,?int $id =null){
+
+        $this->id = $id;
+        $this->name = $name;
+        $this->email     = $email;
+        $this->password  = $password;
+        $this->role      = $role;
+    }
+
+    public function getId(): int { return $this->id; }
+    public function getName(): string { return $this->name; }
+    public function getEmail(): string { return $this->email; }
+    public function getRole(): string { return $this->role; }
+
+
+    public function setId(int $id): int { $this->id = $id; }
+    public function setName(string $name): string { $this->name = $name; }
+    public function setEmail(string $email): string { $this->email = $email; }
+    public function setPassword(string $password): string { $this->password = $password; }
+
+
+
+}
+
+
+
+
+?>
