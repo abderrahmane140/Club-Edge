c3433ca5a83fbe693bdf2021ecf2e3b3ac46cacd
AMINE <amineelhailaa@gmail.com>
2026-01-20 12:52:53 +0100

    add img attribute

--- src/models/Event.php
+++ src/models/Event.php
@@ -11,15 +11,17 @@ class Event
     private string $bio;
     private string $date;
     private string $lieu;
+    private string $img;
 
 
-    public function __construct($titre,$bio,$date,$lieu,$id=null)
+    public function __construct($titre,$bio,$date,$lieu,$img,$id=null)
     {
         $this->id = $id;
         $this->titre = $titre;
         $this->bio = $bio;
         $this->date = $date;
         $this->lieu = $lieu;
+        $this->img = $img;
     }
 
 
@@ -31,6 +33,7 @@ class Event
     public function getBio(){return $this->bio;}
     public function getDate(){return $this->date;}
     public function getLieu(){return $this->lieu;}
+    public function getImg(){return $this->img;}
 
 
 
@@ -42,6 +45,9 @@ class Event
     public function setBio($bio){$this->bio=$bio;}
     public function setDate($date){$this->date=$date;}
     public function setLieu($lieu){$this->lieu=$lieu;}
+    public function setImg($img){
+        $this->img = $img;
+    }
 
 
 
