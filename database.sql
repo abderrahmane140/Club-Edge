ebd1886da1d46fad99c2bb64589fd6973835d56a
ahmed <AbouMalikAhmed98@gmail.com>
2026-01-20 15:08:15 +0100

    !

--- database.sql
+++ database.sql
@@ -1,9 +1,14 @@
+
+CREATE DATABASE club_platform;
+
+
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(20) CHECK (role IN ('student', 'admin')) NOT NULL,
+    role VARCHAR(20) CHECK (role IN ('student','admin')) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
