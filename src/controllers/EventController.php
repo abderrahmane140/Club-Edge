206c0a756339de3d86cd3cae334dd68949d120d2
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-01-21 11:07:29 +0100

    create the controller for the evnts

---
+++ src/controllers/EventController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Src\Controllers;
+
+use Src\core\Controller;
+use Src\Repositories\EventRepository;
+use Src\Models\Event;
+
+class EventController extends Controller
+{
+    private EventRepository $repository;
+
+    public function __construct()
+    {
+        $this->repository = new EventRepository();
+    }
+
+ 
+    public function index()
+    {
+        $events = $this->repository->findAll();
+        $this->view('events/index.php');
+    }
+
+
+    public function show($id)
+    {
+        $event = $this->repository->findById((int)$id);
+
+        if (!$event) {
+            http_response_code(404);
+            $this->view('errors/404.php');
+            return;
+        }
+
+        $this->view('events/show.php');
+    }
+
+ 
+    public function create()
+    {
+        $this->view('events/create.php');
+    }
+
+
+    public function store()
+    {
+        $event = new Event(
+            $_POST['titre'],
+            $_POST['bio'],
+            $_POST['date'],
+            $_POST['lieu'],
+            $_POST['img']
+        );
+
+        $this->repository->create($event);
+
+        $this->redirect('events');
+        exit;
+    }
+
+    public function edit($id)
+    {
+        $event = $this->repository->findById((int)$id);
+        $this->view('events/edit.php');
+    }
+
+
+    public function update($id)
+    {
+        $event = new Event(
+            $_POST['titre'],
+            $_POST['bio'],
+            $_POST['date'],
+            $_POST['lieu'],
+            $_POST['img'],
+            (int)$id
+        );
+
+        $this->repository->update($event);
+
+        $this->redirect('events');
+        exit;
+    }
+
+    public function delete($id)
+    {
+        $this->repository->delete((int)$id);
+
+        $this->redirect('events');
+        exit;
+    }
+}
