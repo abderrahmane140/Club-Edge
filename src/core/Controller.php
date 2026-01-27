02eae464d199655994b971357ce58f2fabfd6687
AMINE <amineelhailaa@gmail.com>
2026-01-27 23:16:13 +0100

    fix sign up redirection

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -12,16 +12,13 @@ abstract class Controller {
 
 
     protected function view(string $view, array $data = []): void {
-        // Remove .blade or .php extension if present
         $view = str_replace(['.blade.php', '.php', '.blade'], '', $view);
         
-        // BladeOne prefers dot notation
         $view = str_replace('/', '.', $view);
         
         $views = __DIR__ . '/../views';
         $cache = __DIR__ . '/../../cache';
 
-        // Use BladeOne for standalone usage
         $blade = new \eftec\bladeone\BladeOne($views, $cache, \eftec\bladeone\BladeOne::MODE_AUTO);
 
         echo $blade->run($view, $data);
